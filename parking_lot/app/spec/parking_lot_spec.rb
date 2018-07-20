d048d2a1062b8c3f144c7d71087ce846fd6dc350
amitgupta <amit.gupta@lifeincontrol.com>
2018-07-20 15:31:17 +0530

    Specs for ParkingLot.new.status

--- parking_lot/app/spec/parking_lot_spec.rb
+++ parking_lot/app/spec/parking_lot_spec.rb
@@ -10,14 +10,14 @@ describe ParkingLot do
     end
 
     context "when parking lot is initialized with no arguments" do
-      it {expect(lambda {ParkingLot.new}).to raise_exception ArgumentError }
+      it {expect(lambda {ParkingLot.new}).to raise_exception ArgumentError}
     end
 
     context "when parking lot is initialize with number of slots other than integer it should raise exception" do
-      it {expect(lambda {ParkingLot.new("6")}).to raise_exception ArgumentError }
-      it {expect(lambda {ParkingLot.new(6.5)}).to raise_exception ArgumentError }
-      it {expect(lambda {ParkingLot.new([1,2])}).to raise_exception ArgumentError }
-      it {expect(lambda {ParkingLot.new({})}).to raise_exception ArgumentError }
+      it {expect(lambda {ParkingLot.new("6")}).to raise_exception ArgumentError}
+      it {expect(lambda {ParkingLot.new(6.5)}).to raise_exception ArgumentError}
+      it {expect(lambda {ParkingLot.new([1, 2])}).to raise_exception ArgumentError}
+      it {expect(lambda {ParkingLot.new({})}).to raise_exception ArgumentError}
     end
 
     context "when parking lot is initialize with proper number of slots as integer" do
@@ -40,13 +40,13 @@ describe ParkingLot do
     end
 
     context "when park is called without any arguments" do
-      it {expect(parking_lot.park()).to raise_exception ArgumentError }
+      it {expect(parking_lot.park()).to raise_exception ArgumentError}
     end
 
     context "when park is called with invalid arguments" do
-      it {expect(parking_lot.park(1,2)).to raise_exception ArgumentError }
-      it {expect(parking_lot.park("KA-01-HH-1234")).to raise_exception ArgumentError }
-      it {expect(parking_lot.park([1,2])).to raise_exception ArgumentError }
+      it {expect(parking_lot.park(1, 2)).to raise_exception ArgumentError}
+      it {expect(parking_lot.park("KA-01-HH-1234")).to raise_exception ArgumentError}
+      it {expect(parking_lot.park([1, 2])).to raise_exception ArgumentError}
     end
 
     context "when park is called with valid arguments i.e registration_number and color of car" do
@@ -77,13 +77,30 @@ describe ParkingLot do
     end
 
     context "when leave is called with correct arguement" do
-      it {expect(parking_lot.leave(1)).to eq($stdout.puts PARKING_LEAVE_SLOT_MESSAGE %{slot: 1} )}
+      it {expect(parking_lot.leave(1)).to eq($stdout.puts PARKING_LEAVE_SLOT_MESSAGE %{slot: 1})}
     end
 
   end
 
   describe '#status' do
+    before :each do
+      parking_lot = ParkingLot.new(1)
+    end
 
+    context "when no car is park" do
+      it {expect(parking_lot.status).to eq($stdout.puts PARKING_EMPTY_MESSAGE)}
+    end
+
+    context "when cars are parked" do
+      it "should return slot with car registration number and color in tablular format" do
+        parking_lot.park("KA-01-HH-1234", "White")
+        expect(parking_lot.status).to end_with(<<-EOTXT
+Slot No.    Registration No    Colour
+1           KA-01-HH-1234      White
+                                      EOTXT )
+
+      end
+    end
   end
 
 end
