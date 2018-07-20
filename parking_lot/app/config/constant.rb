fe452d2adb76174c4ea3100b6f81653ea2be51c8
amitgupta <amit.gupta@lifeincontrol.com>
2018-07-20 14:40:08 +0530

    Spec for ParkingLot.new().park

---
+++ parking_lot/app/config/constant.rb
@@ -0,0 +1,4 @@
+PARKING_FULL_MESSAGE = "Sorry, parking lot is full"
+PARKING_CREATED_MESSAGE = "Created a parking lot with %{slot} slots"
+PARKING_ALLOCATED_MESSAGE = "Allocated slot number: %{slot}"
+PARKING_LEAVE_SLOT_MESSAGE = "Slot number %{slot} is free"
--- parking_lot/app/spec/parking_lot_spec.rb
+++ parking_lot/app/spec/parking_lot_spec.rb
@@ -1,15 +1,16 @@
 require './lib/parking_lot'
+require './config/constant'
 
 describe ParkingLot do
 
   describe '.initialize' do
-    context "when parking lot is initialize with proper number of slots as integer" do
-      before :each do
-        parking_lot = ParkingLot.new(6)
-      end
-      it {expect(parking_lot).to be_instance_of(ParkingLot)}
-      it {expect(parking_lot.slots).not_to be_empty}
-      it {expect(parking_lot.slots.size).to eql(6)}
+
+    context "when ParkingLot can be initialized" do
+      it {expect(ParkingLot).to respond_to(:new)}
+    end
+
+    context "when parking lot is initialized with no arguments" do
+      it {expect(lambda {ParkingLot.new}).to raise_exception ArgumentError }
     end
 
     context "when parking lot is initialize with number of slots other than integer it should raise exception" do
@@ -19,8 +20,41 @@ describe ParkingLot do
       it {expect(lambda {ParkingLot.new({})}).to raise_exception ArgumentError }
     end
 
-    context "when parking lot is initialized with no arguments" do
-      it {expect(lambda {ParkingLot.new}).to raise_exception ArgumentError }
+    context "when parking lot is initialize with proper number of slots as integer" do
+      before :each do
+        parking_lot = ParkingLot.new(6)
+      end
+      it {expect(parking_lot).to be_instance_of(ParkingLot)}
+      it {expect(parking_lot.slots).not_to be_empty}
+      it {expect(parking_lot.slots.size).to eql(6)}
+    end
+
+  end
+
+  describe '#park' do
+    before :each do
+      parking_lot = ParkingLot.new(1)
+    end
+    context "when parking lot has method defined as park" do
+      it {expect(parking_lot).to respond_to(:park)}
+    end
+
+    context "when park is called without any arguments" do
+      it {expect(parking_lot.park()).to raise_exception ArgumentError }
+    end
+
+    context "when park is called with invalid arguments" do
+      it {expect(parking_lot.park(1,2)).to raise_exception ArgumentError }
+      it {expect(parking_lot.park("KA-01-HH-1234")).to raise_exception ArgumentError }
+      it {expect(parking_lot.park([1,2])).to raise_exception ArgumentError }
+    end
+
+    context "when park is called with valid arguments i.e registration_number and color of car" do
+      it {expect(parking_lot.park("KA-01-HH-1234", "white")).to eq($stderr.puts PARKING_ALLOCATED_MESSAGE %{slot: 1})}
+      it "should return parking full message if parking is full" do
+        parking_lot.park("KA-01-HH-1234", "white")
+        expect(parking_lot.park("KA-01-HH-4321", "black")).to eq($stderr.puts PARKING_FULL_MESSAGE)
+      end
     end
 
   end
